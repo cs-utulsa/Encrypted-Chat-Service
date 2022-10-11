088283499b03af2d410b350c570b0287fa04d3b1
Piangno <piangno2007@gmail.com>
2022-10-11 13:25:51 -0500

    Should be working now

--- Encrypted-Chat/client.py
+++ Encrypted-Chat/client.py
@@ -1,20 +1,23 @@
 import socket
 class client():
+    HOST = ""
+    PORT = 0
     def __init__(self, host, port):
         self.HOST = host
         self.PORT = port
     
     def connect(self):
         count = 1
-        myClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
-        myClient.connect((self.HOST, self.PORT))
-        if(myClient.connect == True):
-            return myClient
-        elif(myClient.connect == False and count < 6):
-            count = count + 1
-            self.connect()
-        else:
-            return "null"
+        while(count < 6):
+            try:
+                myClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+                myClient.connect((self.HOST, self.PORT))
+                return myClient
+                break
+            except:
+                count=count+1
+                continue
+        return "null"
         
     def getPort(self):
         return self.PORT
@@ -29,11 +32,15 @@ class client():
         self.HOST = IP
 
 def main():
-    print("kaljsdf;lkj")
-    today = client("127.232.232.11", 1232)
-    if (today.connect == True):
-        print("We are connected")
-    else:
-        print("We are")
-
+    print("Testing for Getters and Setters")
+    today = client("googsadfle.com", 80)
+    print(today.getIP())
+    print(today.getPort())
+    today.setIP("googsadfle.com")
+    print(today.getIP())
+    today.setPort(80)
+    print(today.getPort())
+    today.connect()
+    if(today.connect()=="null"):
+        print("I have null")
 main()
