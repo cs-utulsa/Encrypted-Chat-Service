80988da2f449deb5a57e910f2eae83c2edad0f1a
katesharp370 <56270616+katesharp370@users.noreply.github.com>
2023-02-16 22:56:03 -0600

    Unit Tests

--- Encrypted-Chat/unit_tests.py
+++ Encrypted-Chat/unit_tests.py
@@ -7,23 +7,25 @@ from os.path import exists
 from crypto.encrypt import ECEncrypt
 
 class test(unittest.TestCase):
-    ## Create Encryption class object
     key = Random.new().read(16)
-    enc = ECEncrypt(key,key) ## pass same key twice and encryption and decryption key
+    enc = ECEncrypt(key,key)
 
     def encryptsMessage(self):
+        """KS Unit Test 1: Verifies that the text is being manipulated and does not remain as plaintext""" 
         result = self.enc.encrypt("test")
         original = "test"
         print(original)
         self.assertNotEqual(result, original)
 
     def decryptsMessage(self):
+        """KS Unit Test 2: Verifies that the text is being decrypted correctly"""
         encryptMsg = ECEncrypt.encrypt("test")
         decryptMsg = ECEncrypt.decrypt(encryptMsg)
         original = "test"
         self.assertEqual(decryptMsg, original)  
 
     def unpadMessage(self):
+        """KS Unit Test 3: Verifies that the padding is being removed - WIP"""
         testTxt = "test"
         padData = ECEncrypt._pad(testTxt.decode('utf8'))
         result = ECEncrypt.unpad(padData[16:])
