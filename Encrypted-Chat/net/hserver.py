d1a9f6690b0de9329f4efa1f099b7ad6dbf7acd1
Declan Oberzan <doberzan42@gmail.com>
2023-04-21 00:37:10 -0500

    jk its this one

--- Encrypted-Chat/net/hserver.py
+++ Encrypted-Chat/net/hserver.py
@@ -96,6 +96,7 @@ class EChatServer:
                         if pkt_len > 4096:
                             print("Server read overflow")
                             break
+                        enc_data = ''
                         while len(enc_data) != pkt_len:
                             enc_data += socket.recv(pkt_len-len(enc_data))
                         print("RAW_LEN: ", len(enc_data))
